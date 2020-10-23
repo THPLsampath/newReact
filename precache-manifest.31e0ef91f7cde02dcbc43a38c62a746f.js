501de8a07ec31411f70b171863fe8dc61cb904bc
sampath <sampaththpl@gmail.com>
2020-10-23 10:18:33 +0530

    Updates

---
+++ precache-manifest.31e0ef91f7cde02dcbc43a38c62a746f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41fa8c4976f177488d97a06a3b1c3b0",
+    "url": "/newReact/index.html"
+  },
+  {
+    "revision": "28f611896007cf8677b9",
+    "url": "/newReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f2a783f862d64b0a033c",
+    "url": "/newReact/static/js/2.6ccfc02b.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/newReact/static/js/2.6ccfc02b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28f611896007cf8677b9",
+    "url": "/newReact/static/js/main.605c0cba.chunk.js"
+  },
+  {
+    "revision": "441a01d2df883cda05cc",
+    "url": "/newReact/static/js/runtime-main.4f7cf7f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/newReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
